55702e6e75525da6ead4fde0e198d6d890f67f57
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 12:39:24 -0500

    link index to category pages, make category view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,12 @@
 get '/' do
   # Look in app/views/index.erb
+  @categories = Category.all
   erb :index
+end
+
+get '/:category' do
+  category = Category.find_by_name(params[:category])
+  @posts = category.posts
+
+  erb :category
 end
