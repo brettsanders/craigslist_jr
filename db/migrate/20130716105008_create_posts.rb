4505709048560696c70d51e7fa5d0ff556cd2b1f
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 10:55:46 -0500

    set up models and migrations

---
+++ db/migrate/20130716105008_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :text
+      t.integer :price
+      t.belongs_to :category
+
+
+      t.timestamps
+    end
+  end
+end
